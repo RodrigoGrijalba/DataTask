bb7138304540780012373f5e42d2c2f6edb61a4a
RodrigoGrijalba <ragrijalba119@gmail.com>
2021-10-28 17:09:02 -0500

    Question 1 finished

--- DataProject.py
+++ DataProject.py
@@ -8,6 +8,42 @@ import unidecode as udc
 
 os.chdir('C:/Users\Work\Documents\Proyectos\CU - IDB Data Task')
 
+ratings = pd.read_csv('ratings.csv')
+
+len(ratings.aspect.unique())
+
+len(ratings.worker.unique())
+
+len(ratings) - len(ratings.aspect.unique()) * len(ratings.worker.unique()) # Respondents answered repeat questions 237 times
+
+len_before = len(ratings)
+
+ratings.value_counts(subset = ['aspect', 'worker']).unique()
+
+ratings = ratings.sort_values('time').drop_duplicates(subset = ['worker', 'aspect'], keep = 'last')
+
+rt_after = len(ratings)
+
+len_before - len_after
+
+subjective_riches = ratings.groupby('worker', as_index = 0).rating.mean()
+
+subjective_riches.describe().loc[['min', '25%', '50%', '75%', 'max'],:]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
