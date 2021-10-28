afc709eaa5cbdddfdc5c0f27ed07cbaea3987cb0
RodrigoGrijalba <ragrijalba119@gmail.com>
2021-10-28 15:41:48 -0500

    First commit

---
+++ DataProject.py
@@ -0,0 +1,15 @@
+
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import shapefile as shp
+import unidecode as udc
+
+os.chdir('C:/Users\Work\Documents\Proyectos\CU - IDB Data Task')
+
+
+
+
+
+
